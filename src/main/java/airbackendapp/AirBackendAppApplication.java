886b84c56a76afabca565e82a068cd7b6fe9a576
NJOMBOSS <fonke1982@gmail.com>
2025-02-09 20:37:50 +0100

    Configuration de la base de données

---
+++ src/main/java/airbackendapp/AirBackendAppApplication.java
@@ -0,0 +1,13 @@
+package airbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBackendAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirBackendAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/airbackendapp/AirBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package airbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBackendAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
